c2f12c34a77f7bc0f6487e20c2015bd2196a3f63
Mariusz Kurpik <mqrpik@gmail.com>
2018-10-22 14:53:57 +0200

    small changes in destription

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,11 +5,11 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3'>
-                {'This simply App detect faces in your pictures.'}
-            </p>
+                {'This simply App detect face in your pictures. '}
+            </p> <a target="_blank" href="https://clarifai.com">Powered by clarifai</a>
             <div className='center'>
             <div className='center form pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='tex' 
+                <input className='f4 pa2 w-70 center' type='tex' placeholder="Link to your picture"
                 onChange={onInputChange} 
                  />
                 <button className='w-30 grow f4 link ph3 pv2 dib white bg-dark-blue' 
